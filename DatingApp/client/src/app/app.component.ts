561d9d03de537696e3a50859b380f8b387297664
piotr-skolorzynski <p.skolorzynski@gmail.com>
2024-12-22 18:34:54 +0100

    Add CORS to API project to let angular app use endpoints Add http get request in angular app to fetch users

--- DatingApp/client/src/app/app.component.ts
+++ DatingApp/client/src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'Dating App';
+export class AppComponent implements OnInit {
+  public title = 'Dating App';
+  public users: any;
+  private readonly http = inject(HttpClient);
+
+  public ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/users').subscribe({
+      next: response => (this.users = response),
+      error: error => console.log(error),
+      complete: () => console.log('Request has completed'),
+    });
+  }
 }
