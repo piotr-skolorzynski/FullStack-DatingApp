1a6bbc3c235309da21df1cdc5975dc2670617f9a
piotr-skolorzynski <p.skolorzynski@gmail.com>
2024-12-23 13:32:57 +0100

    Create AccountController with Register endpoint using RegisterDto

--- DatingApp/API/Controllers/BaseApiController.cs
+++ DatingApp/API/Controllers/BaseApiController.cs
@@ -1,10 +1,11 @@
 using Microsoft.AspNetCore.Mvc;
 
-namespace API.Controllers;
-
-[ApiController]
-[Route("api/[controller]")]
-public class BaseApiController: ControllerBase
+namespace API.Controllers
 {
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BaseApiController: ControllerBase
+    {
 
+    }
 }
