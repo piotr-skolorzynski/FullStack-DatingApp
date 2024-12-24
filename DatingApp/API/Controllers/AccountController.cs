03ceef8c698708d3925d1ad266afe38371235e39
piotr-skolorzynski <p.skolorzynski@gmail.com>
2024-12-24 10:08:20 +0100

    Create UserDto Refactor AccountController to use TokenService and UserDto

--- DatingApp/API/Controllers/AccountController.cs
+++ DatingApp/API/Controllers/AccountController.cs
@@ -2,16 +2,17 @@ using System.Security.Cryptography;
 using System.Text;
 using API.Data;
 using API.DTOs;
+using API.Interfaces;
 using API.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    public class AccountController(DataContext context) : BaseApiController
+    public class AccountController(DataContext context, ITokenService tokenService) : BaseApiController
     {
         [HttpPost("register")] //api/register
-        public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto) 
+        public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto) 
         {
             if (await UserExists(registerDto.Username)) return BadRequest("Username is taken");
 
@@ -28,11 +29,15 @@ namespace API.Controllers
             context.Users.Add(user);
             await context.SaveChangesAsync();
 
-            return user;
+            return new UserDto
+            {
+                Username = user.UserName,
+                Token = tokenService.CreateToken(user)
+            };
         }
 
         [HttpPost("login")]
-        public async Task<ActionResult<AppUser>> Login(LoginDto loginDto) 
+        public async Task<ActionResult<UserDto>> Login(LoginDto loginDto) 
         {
             var user = await context.Users.FirstOrDefaultAsync(x => x.UserName == loginDto.Username.ToLower());
 
@@ -43,14 +48,16 @@ namespace API.Controllers
 
             var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
 
-            //pętla po tablicy bajtów
             for (int i = 0; i < computedHash.Length; i++)
             {
                 if (computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid password"); //zmienić później treść
             }
 
-            //jeśli wyjdziemy z pętli to wiemy że jesteśmy uwierzytelnieni
-            return user;
+            return new UserDto
+            {
+                Username = user.UserName,
+                Token = tokenService.CreateToken(user)
+            };
         }
 
         private async Task<bool> UserExists(string username)
