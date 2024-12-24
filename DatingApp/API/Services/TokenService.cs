7c0ad8743a12b76a8cb1fd6c3ae6696187244e70
piotr-skolorzynski <p.skolorzynski@gmail.com>
2024-12-24 09:04:56 +0100

    Create ITokenService interface Cerate TokenService implementing created interface Register service in dependency injection container

---
+++ DatingApp/API/Services/TokenService.cs
@@ -0,0 +1,12 @@
+using API.Interfaces;
+using API.Models;
+
+namespace API.Services;
+
+public class TokenService : ITokenService
+{
+    public string CreateToken(AppUser user)
+    {
+        throw new NotImplementedException();
+    }
+}
