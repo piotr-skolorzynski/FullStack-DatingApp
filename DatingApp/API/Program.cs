35314887fe45bf7f133a7b87922c835ffc79cd1e
piotr-skolorzynski <p.skolorzynski@gmail.com>
2024-12-25 10:28:56 +0100

    Create ApplicationServiceExtensions to extract register services to separated file

--- DatingApp/API/Program.cs
+++ DatingApp/API/Program.cs
@@ -1,25 +1,16 @@
 using System.Text;
-using API.Data;
-using API.Interfaces;
-using API.Services;
+using API.Extensions;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddApplicationServices(builder.Configuration);
 
-builder.Services.AddControllers();
-builder.Services.AddDbContext<DataContext>(opt => 
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
 {
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-builder.Services.AddCors();
-builder.Services.AddScoped<ITokenService, TokenService>();
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options => 
-{
-    var tokenKey = builder.Configuration["TokenKey"] ?? throw new Exception("TokenKey not found"); //ten sam klucz bo używamy szyfrowania symetrycznego
+    var tokenKey = builder.Configuration["TokenKey"] ?? throw new Exception("TokenKey not found");
     options.TokenValidationParameters = new TokenValidationParameters
     {
         ValidateIssuerSigningKey = true, //nie pozwoli na przyjęcie dowolnego tokena
@@ -35,7 +26,7 @@ var app = builder.Build();
 app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200", "https://localhost:4200"));
 
 app.UseAuthentication();
-app.UseAuthorization();    
+app.UseAuthorization();
 
 app.MapControllers();
 
