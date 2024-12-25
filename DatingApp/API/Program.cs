37a2c7eda2ade73ad16a0401190387c1631addbe
piotr-skolorzynski <p.skolorzynski@gmail.com>
2024-12-25 10:41:50 +0100

    Create IdentityServiceExtensions Extract logic connected with authentication from Program.cs file

--- DatingApp/API/Program.cs
+++ DatingApp/API/Program.cs
@@ -1,24 +1,11 @@
-using System.Text;
 using API.Extensions;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddApplicationServices(builder.Configuration);
 
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
-{
-    var tokenKey = builder.Configuration["TokenKey"] ?? throw new Exception("TokenKey not found");
-    options.TokenValidationParameters = new TokenValidationParameters
-    {
-        ValidateIssuerSigningKey = true, //nie pozwoli na przyjęcie dowolnego tokena
-        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenKey)),
-        ValidateIssuer = false,
-        ValidateAudience = false
-    };
-});
+builder.Services.AddIdentityServices(builder.Configuration);
 
 var app = builder.Build();
 
