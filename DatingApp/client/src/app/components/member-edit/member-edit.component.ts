25c95e35a51a4fe13bc4db19fb6a6110ec377065
piotr-skolorzynski <p.skolorzynski@gmail.com>
2025-01-05 18:30:36 +0100

    Modify MemberEdit component Add template with reactive form to display member data ready to be edited

--- DatingApp/client/src/app/components/member-edit/member-edit.component.ts
+++ DatingApp/client/src/app/components/member-edit/member-edit.component.ts
@@ -1,19 +1,43 @@
-import { Component, inject } from '@angular/core';
-import { AccountService, MembersService } from '../../services';
+import { Component, computed, effect, inject } from '@angular/core';
 import { rxResource } from '@angular/core/rxjs-interop';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { MembersService, AccountService } from '../../services';
 
 @Component({
   selector: 'app-member-edit',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './member-edit.component.html',
   styleUrl: './member-edit.component.css',
 })
 export class MemberEditComponent {
   private readonly memberService = inject(MembersService);
   private readonly username = inject(AccountService).currentUser;
+  private readonly fb = inject(FormBuilder);
 
+  public memberForm: FormGroup;
   public member = rxResource({
     request: () => this.username,
     loader: () => this.memberService.getMember(this.username()?.username ?? ''),
+  });
+  public fillForm = effect(() => {
+    const member = this.member.value();
+
+    this.memberForm = this.fb.group({
+      introduction: this.fb.control(member?.introduction ?? '', {
+        validators: [],
+      }),
+      lookingFor: this.fb.control(member?.lookingFor ?? '', {
+        validators: [],
+      }),
+      interests: this.fb.control(member?.interests ?? '', {
+        validators: [],
+      }),
+      city: this.fb.control(member?.city ?? '', {
+        validators: [],
+      }),
+      country: this.fb.control(member?.country ?? '', {
+        validators: [],
+      }),
+    });
   });
 }
