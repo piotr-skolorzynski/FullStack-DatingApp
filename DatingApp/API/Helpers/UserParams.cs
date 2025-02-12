edeaeb785285f8272d6547c5ab8fabe20b01b0f8
piotr-skolorzynski <p.skolorzynski@gmail.com>
2025-02-12 19:49:29 +0100

    Create helper classes for pagination

---
+++ DatingApp/API/Helpers/UserParams.cs
@@ -0,0 +1,15 @@
+namespace API.Helpers;
+
+public class UserParams
+{
+    //zazwyczaj ustawiamy ręcznie maksymalną liczbę
+    private const int MaxPageSize = 50;
+    public int PageNumber { get; set; } = 1;
+    private int _pageSize = 10; //domyślny rozmiar strony
+
+    public int PageSize
+    {
+        get => _pageSize;
+        set => _pageSize = (value > MaxPageSize) ? MaxPageSize : value;
+    }
+}
