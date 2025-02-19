75e0bcc0055b664c86c32622bdf063045ac53ba2
piotr-skolorzynski <p.skolorzynski@gmail.com>
2025-02-19 18:08:49 +0100

    Add filterin buttons in member list component

--- DatingApp/client/src/app/components/member-list/member-list.component.ts
+++ DatingApp/client/src/app/components/member-list/member-list.component.ts
@@ -1,19 +1,26 @@
 import { Component, inject, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { PageChangedEvent, PaginationModule } from 'ngx-bootstrap/pagination';
 import { AccountService, MembersService } from '../../services';
-import { MemberCardComponent } from '../member-card/member-card.component';
 import { UserParams } from '../../models';
+import { Gender } from '../../enums';
+import { MemberCardComponent } from '../member-card/member-card.component';
 
 @Component({
   selector: 'app-member-list',
-  imports: [MemberCardComponent, PaginationModule],
+  imports: [MemberCardComponent, PaginationModule, FormsModule],
   templateUrl: './member-list.component.html',
   styleUrl: './member-list.component.css',
 })
 export class MemberListComponent implements OnInit {
   private readonly accountService = inject(AccountService);
-  public readonly memberService = inject(MembersService);
-  private userParams = new UserParams(this.accountService.currentUser());
+  private readonly memberService = inject(MembersService);
+  public userParams = new UserParams(this.accountService.currentUser());
+  public paginatedResult = this.memberService.paginatedResult;
+  public genderList = [
+    { value: Gender.Male, display: 'Males' },
+    { value: Gender.Female, display: 'Females' },
+  ];
 
   public ngOnInit(): void {
     if (!this.memberService.paginatedResult()) {
@@ -28,7 +35,12 @@ export class MemberListComponent implements OnInit {
     }
   }
 
-  private loadMembers(): void {
+  public resetFilters(): void {
+    this.userParams = new UserParams(this.accountService.currentUser());
+    this.loadMembers();
+  }
+
+  public loadMembers(): void {
     this.memberService.getMembers(this.userParams);
   }
 }
