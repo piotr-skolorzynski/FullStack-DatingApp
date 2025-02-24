bb5a12775b8a17d206b95d637111daa9dc4e449a
piotr-skolorzynski <p.skolorzynski@gmail.com>
2025-02-24 19:58:59 +0100

    Create new Likes entity

--- DatingApp/API/Data/DataContext.cs
+++ DatingApp/API/Data/DataContext.cs
@@ -6,4 +6,27 @@ namespace API.Data;
 public class DataContext(DbContextOptions options) : DbContext(options)
 {
     public required DbSet<AppUser> Users { get; set; }
+    public DbSet<UserLike> Likes { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        builder.Entity<UserLike>()
+            .HasKey(k => new { k.SourceUserId, k.TargetUserId });
+
+        builder.Entity<UserLike>()
+            .HasOne(s => s.SourceUser)
+            .WithMany(l => l.LikedUsers)
+            .HasForeignKey(s => s.SourceUserId)
+            .OnDelete(DeleteBehavior.NoAction);
+        //.OnDelete(DeleteBehavior.Cascade); //SQL serwer nie lubi tego, dla niego trzeba dedykować NoAction
+
+        builder.Entity<UserLike>()
+            .HasOne(s => s.TargetUser)
+            .WithMany(l => l.LikedByUsers)
+            .HasForeignKey(s => s.TargetUserId)
+            .OnDelete(DeleteBehavior.NoAction);
+        // .OnDelete(DeleteBehavior.Cascade);
+    }
 }
---
+++ DatingApp/API/Models/UserLike.cs
@@ -0,0 +1,9 @@
+namespace API.Models;
+
+public class UserLike
+{
+    public AppUser SourceUser { get; set; } = null!;
+    public int SourceUserId { get; set; }
+    public AppUser TargetUser { get; set; } = null!;
+    public int TargetUserId { get; set; }
+}
