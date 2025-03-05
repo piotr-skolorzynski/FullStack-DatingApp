fa9145eb871ef5393ecf4ef7a1a121e745e6180d
piotr-skolorzynski <p.skolorzynski@gmail.com>
2025-03-05 21:40:43 +0100

    Adding a message controller

---
+++ DatingApp/API/Controllers/MessagesController.cs
@@ -0,0 +1,39 @@
+using API.DTOs;
+using API.Extensions;
+using API.Interfaces;
+using API.Models;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+public class MessagesController(IMessageRepository messageRespository, IUserRepository userRepository, IMapper mapper) : BaseApiController
+{
+    [HttpPost]
+    public async Task<ActionResult<MessageDto>> CreateMessage(CreateMessageDto createMessageDto)
+    {
+        var username = User.GetUsername();
+
+        if (username == createMessageDto.RecipientUsername.ToLower()) return BadRequest("You cannot message yourself");
+
+        var sender = await userRepository.GetUserByUsernameAsync(username);
+        var recipient = await userRepository.GetUserByUsernameAsync(createMessageDto.RecipientUsername);
+
+        if (recipient == null || sender == null) return BadRequest("Cannot send message at this time");
+
+        var message = new Message
+        {
+            Sender = sender,
+            Recipient = recipient,
+            SenderUsername = sender.UserName,
+            RecipientUsername = recipient.UserName,
+            Content = createMessageDto.Content
+        };
+
+        messageRespository.AddMessage(message);
+
+        if (await messageRespository.SaveAllAsync()) return Ok(mapper.Map<MessageDto>(message));
+
+        return BadRequest("Failed to save message");
+    }
+}
