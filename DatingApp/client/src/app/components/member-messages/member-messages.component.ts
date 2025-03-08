bf192184c54d3405c5a13fa3e01856ea9ce7176f
piotr-skolorzynski <p.skolorzynski@gmail.com>
2025-03-08 19:27:56 +0100

    Style the message thread

--- DatingApp/client/src/app/components/member-messages/member-messages.component.ts
+++ DatingApp/client/src/app/components/member-messages/member-messages.component.ts
@@ -1,12 +1,15 @@
 import { Component, inject, input, OnInit } from '@angular/core';
 import { MessageService } from '../../services/message.service';
+import { TimeagoModule } from 'ngx-timeago';
 
 @Component({
   selector: 'app-member-messages',
   templateUrl: './member-messages.component.html',
   styleUrl: './member-messages.component.css',
+  imports: [TimeagoModule],
 })
 export class MemberMessagesComponent {
+  public username = input.required<string>();
   private readonly messageService = inject(MessageService);
 
   public threadMessages = this.messageService.threadMessages;
