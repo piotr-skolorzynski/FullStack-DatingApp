b2f9b477b675bfb565e186bf998baebd1931c988
piotr-skolorzynski <p.skolorzynski@gmail.com>
2025-03-15 14:52:59 +0100

    Get yhe users with roles

--- DatingApp/API/Controllers/AdminController.cs
+++ DatingApp/API/Controllers/AdminController.cs
@@ -1,15 +1,29 @@
+using System.Threading.Tasks;
+using API.Models;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    public class AdminController : BaseApiController
+    public class AdminController(UserManager<AppUser> userManager) : BaseApiController
     {
         [Authorize(Policy = "RequireAdminRole")]
         [HttpGet("users-with-roles")]
-        public ActionResult GetUsersWithRoles()
+        public async Task<ActionResult> GetUsersWithRoles()
         {
-            return Ok("Only admins can see this");
+            var users = await userManager.Users
+                .OrderBy(x => x.UserName)
+                .Select(x => new
+                {
+                    x.Id,
+                    Username = x.UserName,
+                    Roels = x.UserRoles.Select(r => r.Role.Name).ToList()
+                })
+                .ToListAsync();
+
+            return Ok(users);
         }
 
         [Authorize(Policy = "ModeratePhotoRole")]
