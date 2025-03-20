d5ff12e1254d00cd19e50f88b89dde5024770bfb
piotr-skolorzynski <p.skolorzynski@gmail.com>
2025-03-20 22:28:23 +0100

    Add a presence tracker

--- DatingApp/API/Extensions/ApplicationServiceExtensions.cs
+++ DatingApp/API/Extensions/ApplicationServiceExtensions.cs
@@ -2,6 +2,7 @@ using API.Data;
 using API.Helpers;
 using API.Interfaces;
 using API.Services;
+using API.SignalR;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Extensions;
@@ -25,6 +26,7 @@ public static class ApplicationServiceExtensions
         services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
         services.Configure<CloudinarySettings>(config.GetSection("CloudinarySettings"));
         services.AddSignalR();
+        services.AddSingleton<PresenceTracker>();
 
         return services;
     }
