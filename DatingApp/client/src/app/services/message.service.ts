cee65115908e07b3b4f4ce08ec522c756b84c3f0
piotr-skolorzynski <p.skolorzynski@gmail.com>
2025-03-23 18:28:34 +0100

    Add the message hub connection to the message service on the client side

--- DatingApp/client/src/app/services/message.service.ts
+++ DatingApp/client/src/app/services/message.service.ts
@@ -1,20 +1,28 @@
 import { computed, inject, Injectable, signal } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { rxResource } from '@angular/core/rxjs-interop';
-import { Observable, tap } from 'rxjs';
+import { tap } from 'rxjs';
+import {
+  HubConnection,
+  HubConnectionBuilder,
+  HubConnectionState,
+} from '@microsoft/signalr';
 import { environment } from '../../environments/environment';
 import { IMessagesParams } from '../interfaces/messages-params.interface';
 import { setPaginationHeaders } from './pagination-helpers';
-import { IMessage, IPagination } from '../interfaces';
+import { IMessage, IPagination, IUser } from '../interfaces';
 
 @Injectable({
   providedIn: 'root',
 })
 export class MessageService {
   private readonly baseUrl = environment.apiUrl;
+  private readonly hubUrl = environment.hubsUrl;
   private readonly http = inject(HttpClient);
+  private hubConnection?: HubConnection;
   public messagesPagination = signal<IPagination | null>(null);
   public usernameForThread = signal<string>('');
+  public messageThread = signal<IMessage[]>([]);
 
   public messagesParams = signal<IMessagesParams>({
     container: 'unread',
@@ -76,4 +84,25 @@ export class MessageService {
         `${this.baseUrl}messages/thread/${request.username}`
       ),
   });
+
+  private createHubConnection(user: IUser, otherUsername: string): void {
+    this.hubConnection = new HubConnectionBuilder()
+      .withUrl(`${this.baseUrl}message?user=${otherUsername}`, {
+        accessTokenFactory: () => user.token,
+      })
+      .withAutomaticReconnect()
+      .build();
+
+    this.hubConnection.start().catch(error => console.log(error));
+
+    this.hubConnection.on('ReceivedMessageThread', messages =>
+      this.messageThread.set(messages)
+    );
+  }
+
+  private stopHubConnection(): void {
+    if (this.hubConnection?.state === HubConnectionState.Connected) {
+      this.hubConnection.stop().catch(error => console.log(error));
+    }
+  }
 }
